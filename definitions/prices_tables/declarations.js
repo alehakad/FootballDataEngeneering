1ba3385231138396b3d3b65830448358591cee7e
Aleksei Kadyrov <alehak2001@gmail.com>
2025-03-31 10:44:51 +0000

    Started adding price players data

---
+++ definitions/prices_tables/declarations.js
@@ -0,0 +1,17 @@
+declare({
+  database: "footballdataengineering",
+  schema: "players_prices",
+  name: "player_data"
+});
+
+declare({
+  database: "footballdataengineering",
+  schema: "players_prices",
+  name: "club_data"
+});
+
+declare({
+  database: "footballdataengineering",
+  schema: "helpers",
+  name: "team_name_mapping"
+});
